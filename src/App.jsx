86d8d45b8d84a09f6cf6924957862a48e80f2d68
Stanislau Shliakhtsich <stasgm@gmail.com>
2017-11-10 00:07:25 +0300

    add new features

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,41 @@ import Gallery from './components/Gallery';
 import Profile from './components/Profile';
 import './App.css';
 import $ from 'jquery';
+import fetchJsonp from 'fetch-jsonp';
 
 
-const ACCESS_TOKEN = '220968095.60ee6b5.fa4881ace5a049799d3b1342699302d9';
+
+//const INSTAGRAM_ID = 220968095;
+//const INSTAGRAM_ID = 3567862449; //ardent_arctic
 
 class App extends Component{
     constructor(props){
         super(props);
         this.state = {
-            status :' ',
+            status : '',
+            pagination: 1,
+            current_id: 3567862449,
+            id_list: [3567862449, 220968095],
+            access_token: '220968095.60ee6b5.fa4881ace5a049799d3b1342699302d9',
             profile: {
-                id: null,
-                username: 'Stas',
-                full_name : 'Stas',
+                id: 0,
+                username: '',
+                full_name : '',
                 profile_picture: 'https://instagram.fiev1-1.fna.fbcdn.net/t51.2885-15/e35/23279419_1870135956347978_250684463539814400_n.jpg',
-                followers: 0,
-                follow: 0,
-                image_count : 0
+                counts: {
+                    followed_by: 0,
+                    follows: 0,
+                    media : 0
+                }
             },
-            images: [{
-                title: 'test',
-                url : ''
-            }]
+            media: []
         }
+    }
+
+    componentDidMount() {
+        this.showMessage();
+        this.loadProfile(this.state.id_list[0]);
+        this.loadRecentMedia();
     }
 
     showMessage(){
@@ -50,7 +62,7 @@ class App extends Component{
             url: 'https://api.instagram.com/v1/users/search?q=damned_god',
             dataType: 'jsonp',
             type: 'GET',
-            data: {access_token: ACCESS_TOKEN},
+            data: {access_token: this.state.access_token},
             success: function(data){
                 const profile = data.data[0];
                 this.setState({ profile });
@@ -61,9 +73,68 @@ class App extends Component{
         });
     };
 
-    search() {
+    //&access_token=220968095.60ee6b5.fa4881ace5a049799d3b1342699302d9
+
+
+    loadProfile(id){
+        this.setState({current_id: id});
+        this.setState({pagination: 1});
+
+        const url = `https://api.instagram.com/v1/users/${id}/?access_token=${this.state.access_token}`;
+        fetchJsonp(url)
+            .then(function(response) {
+                return response.json()
+            }).then(function(json) {
+            const profile = json.data;
+            this.setState({ profile });
+        }.bind(this)).catch(function(ex) {
+//            console.log('parsing failed', ex);
+            this.setState({status: 'parsing failed' + ex});
+        }.bind(this))
+
+        //this.loadRecentMedia();
+    }
+    loadProfile_old() {
         /* Загрузка информации о профиле*/
-        this.GetID();
+        //this.GetID();
+
+
+        //console.log(this.state.profile.id);
+        $.ajax({
+            url: `https://api.instagram.com/v1/users/${this.state.current_id}/`,
+            dataType: 'jsonp',
+            type: 'GET',
+            data: {access_token: this.state.access_token},
+            success: function(data){
+                const profile = data.data;
+                this.setState({ profile });
+            }.bind(this),
+            error: function(error){
+                this.setState({status: error.message});
+            }.bind(this),
+        });
+    }
+
+
+    loadRecentMedia() {
+        /* Загрузка последних 10 фотографий*/
+        //this.GetID();
+        this.setState({pagination: this.state.pagination + 1});
+        //console.log(this.state.profile.id);
+        //console.log(this.state.pagination);
+        $.ajax({
+            url: `https://api.instagram.com/v1/users/${this.state.current_id}/media/recent/?count=${this.state.pagination*5}`,
+            dataType: 'jsonp',
+            type: 'GET',
+            data: {access_token: this.state.access_token},
+            success: function(data){
+                const media = data.data;
+                this.setState({ media });
+            }.bind(this),
+            error: function(error){
+                this.setState({status: error.message});
+            }.bind(this),
+        });
     }
 
     render(){
@@ -76,16 +147,32 @@ class App extends Component{
                     <button className='Tags-button-show-message button' onClick={() => this.showMessage()}>
                         Загрузить цитату
                     </button>
-                    <div className='status'>{this.state.status}</div>
-                    <button className='Tags-button-show-profile button' onClick={() => this.search()}>
+                    {/*
+                    <button className='Tags-button-show-profile button' onClick={() => this.loadProfile()}>
                         Загрузить профиль
                     </button>
+                    */}
+                    <button className='Tags-button-show-profile button' onClick={() => this.loadRecentMedia()}>
+                        Загрузить 5 фото
+                    </button>
+                    <button className='Tags-button-show-profile button' onClick={() => this.loadProfile(this.state.id_list[1])}>
+                        Загрузка Stas
+                    </button>
+                    <button className='Tags-button-show-profile button' onClick={() => this.loadProfile(this.state.id_list[0])}>
+                        Загрузка Alexandra
+                    </button>
+                    <select name="profile">
+                        <option value={this.state.id_list[0]}>Alexandra</option>
+                        <option value={this.state.id_list[0]}>Stas</option>
+                    </select>
+
+                    <div className='status'>{this.state.status}</div>
                     {/*
                       <input className='Tags-input' placeholder='Введите теги' />
                     */}
                     <div>
                         <Profile profile={this.state.profile} />
-                        <Gallery images={this.state.images} count={this.state.images.length} />
+                        <Gallery images={this.state.media} count={this.state.media.length} pagination={this.state.pagination}/>
                     </div>
                 </div>
             </div>
